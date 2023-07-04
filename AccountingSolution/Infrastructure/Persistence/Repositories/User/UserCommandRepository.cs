ad8d406c95ff6fb01c0fddf40677d56757937e96
saeid atefi <atefi.saeid@gmail.com>
2023-07-04 14:48:07 +0330

    jwt-2

--- AccountingSolution/Infrastructure/Persistence/Repositories/User/UserCommandRepository.cs
+++ AccountingSolution/Infrastructure/Persistence/Repositories/User/UserCommandRepository.cs
@@ -15,13 +15,11 @@ namespace Infrastructure.Persistence.Repositories
 {
     public class UserCommandRepository: IUserCommandRepository
     {
-        private readonly UserManager<Infrastructure.Persistence.Entities.ApplicationUser> _userManager;
-        private readonly SignInManager<Infrastructure.Persistence.Entities.ApplicationUser> _signInManager;
+
         private readonly IMapper _mapper;
-        public UserCommandRepository(UserManager<Infrastructure.Persistence.Entities.ApplicationUser> userManager, SignInManager<Infrastructure.Persistence.Entities.ApplicationUser> signInManager, IConfiguration configuration, IMapper mapper)
+        public UserCommandRepository(  IMapper mapper)
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
+        
             _mapper = mapper;
         }
 
@@ -29,18 +27,8 @@ namespace Infrastructure.Persistence.Repositories
         public async Task<UserModel> GetUserByUsernameandPassword(string Username,string Password)
         {
            
-            var user = await _userManager.FindByNameAsync(Username);
-            if (user != null)
-            {
-                var checkResult = await _signInManager.CheckPasswordSignInAsync(user, Password, lockoutOnFailure: false);
-                if (!checkResult.Succeeded)
-                {
-                    return null;
-                }              
-            }
-
-            var result = _mapper.Map<UserModel>(user);
-            return result;
+          
+            return null;
 
         }
 
